611bae274bc68e2a5c8576ff91e848eccb7450c4
Felix Tudoran <felix.tudoran@snhu.edu>
2020-06-13 21:02:59 -0700

    Added changes for the algorithms and data structures milestone

--- capstone_project/management.py
+++ capstone_project/management.py
@@ -1,27 +1,53 @@
 from DbConnection import DbConnection
-
+import json
 
 class Trader:
     def __init__(self):
         self.__str__ = "Trader"
         self.email = ""
         self.company = ""
-        self.db = DbConnection('market', 'stocks')
+        self.db = DbConnection('market', 'stocks')    # create database connection object
+        self.menu_options = [x for x in dir(self) if x.startswith("m_")]  # create options menu for user using class attributes
 
-    def get_industry_tickers(self, industry):
+        #menu = {}
+        #menu_item_number = 0
+        #for item in self.menu_options:
+        #    menu[menu_item_number] = item
+        #    menu_item_number += 1
+        #for option_num, option_name in menu.items():
+        #    print("%s. %s" % (option_num, option_name))
+
+        while True:
+            # get input from user prompt and call the appropriate function name 
+            menu = input("\nOptions:\n=============\nEnter name of option and press ENTER, or 'q' to quit:\n\n%s\n>" % "\n".join(self.menu_options))
+            if menu == "q":
+                print("Good bye!")
+                return
+            try:
+                func = getattr(self, menu)
+                print(func())
+            except AttributeError:
+                print("Invalid option name")
+
+    def m_get_industry_tickers(self):
         """Get Industry tickers"""
+        industry = input("Enter name of industry and press ENTER: ")
         return self.db.get_industry_tickers(industry)
 
-    def get_50_day_simple_moving_avg_count(self, low, high):
+    def m_get_50_day_simple_moving_avg_count(self):
         """Get 50 day simple moving average count"""
+        low = input("Enter lower threshold value for 50 day average count and press ENTER: ")
+        high = input("Enter high threshold value for 50 day average count and press ENTER: ")
         return self.db.get_50_day_simple_moving_avg_count(low, high)
 
-    def get_total_outstanding_shares_by_sector(self, sector):
+    def m_get_total_outstanding_shares_by_sector(self):
         """Get total outstanding shares by sector"""
+        sector = input("Enter name of sector and press ENTER: ")
         return self.db.get_total_outstanding_shares_by_sector(sector)
 
-    def get_analyst_recommendation_score(self, ticker):  # new function
+    def m_get_analyst_recommendation_score(self):  # new function
         """Get tickers recommended by analysts where score greater than low"""
+        ticker = input("Enter name of ticker and press ENTER: ")
         return self.db.get_analyst_recommendation_score(ticker)
 
 
@@ -29,20 +55,28 @@ class Manager(Trader):
     def __init__(self):
         super().__init__()
         self.__str__ = "Hedge Fund Manager"
-        self.managed_tickers = []
+                                 
 
-    def create_document(self, document):
+    def m_create_document(self):
         """Create document"""
+        document = input("Enter Document in key, value pair format and press ENTER: ")
         return self.db.create_document(document)
 
-    def update_volume(self, ticker, volume):
+    def m_update_volume(self):
         """Update ticker volume"""
+        ticker = input("Enter ticker symbol and press ENTER: ")
+        volume = input("Enter volume for ticker and press ENTER: ")
         return self.db.update_volume(ticker, volume)
 
-    def update_document(self, document):  # new function
+    def m_update_document(self):  # new function
         """Update document"""
+        document = input("Enter Document in key, value pair format and press ENTER: ")
         return self.db.update_document(document)
 
-    def delete_document(self, ticker):
+    def m_delete_document(self):
         """Delete document"""
+        ticker = input("Enter ticker symbol and press ENTER: ")
         return self.db.delete_document(ticker)
+
+
+
---
+++ capstone_project/tests.py
@@ -0,0 +1,47 @@
+from mock import mock
+import unittest
+import chr2hex
+from final_api import request_prompt
+from DbConnection import DbConnection
+
+class CapstoneTestCase(unittest.TestCase):
+    def setUp(self):
+        self.db = DbConnection("foo_db", "foo_coll")
+
+    def test_chr2hex_encode(self):
+        self.assertEqual(chr2hex.encode("snhucapstone"), "736E687563617073746F6E65")
+
+    def test_chr2hex_decode(self):
+        self.assertEqual(chr2hex.decode("736E687563617073746F6E65"), "snhucapstone")
+
+    def test_request_prompt(self):
+        request_prompt.input = mock.Mock()
+        request_prompt.re = mock.Mock()
+        request_prompt.return_value = "m"
+        self.assertEqual("m", request_prompt.return_value)
+        request_prompt.return_value = "t"
+        self.assertEqual("t", request_prompt.return_value)
+
+    def test_create_document(self):
+        self.db.collection.insert_one = mock.Mock()
+        result = self.db.create_document('{"FOO": 1000}')
+        self.assertEqual(self.db.create_document('{"FOO": 1000}'), result)
+
+    def test_update_volume(self):
+        self.db.collection.update_one = mock.Mock()
+        self.db.collection.find_one = mock.Mock()
+        self.db.collection.find_one.return_value = {"FOO: 2000"}
+        self.assertEqual(self.db.update_volume("FOO", 2000), self.db.collection.find_one.return_value)
+
+    def test_delete_document_by_ticker(self):
+        self.db.collection.find_one_and_delete = mock.Mock()
+        self.db.collection.find_one_and_delete.return_value = dict({"FOO": 1000})
+        self.assertEqual(self.db.collection.find_one_and_delete.return_value, self.db.delete_document("FOO"))
+
+    def test_get_industry_tickers(self):
+        self.db.collection.find = mock.Mock()
+        self.db.collection.find.return_value = [{"Industry": "Semiconductors", "Ticker": "AMD"}, {"Industry": "Semiconductors", "Ticker": "INTL"}]
+        self.assertEqual(self.db.get_industry_tickers("Semiconductors"), ['AMD', 'INTL'])
+
+if __name__ == '__main__':
+    unittest.main()
